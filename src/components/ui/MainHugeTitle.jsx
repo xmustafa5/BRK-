088b874ae0c3531d3d1e2c166c4883c587ec2cb6
Mustafa mohammed <107437940+xmustafa5@users.noreply.github.com>
2024-02-17 16:10:11 +0300

    fix mainHugeTRitle

--- src/components/ui/MainHugeTitle.jsx
+++ src/components/ui/MainHugeTitle.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function MainHugeTitle({ title, haveSpan = false }) {
+  return(
     <h2 className="flex Extrabold flex-col gap-3 mt-4 text-white text-7xl font-semibold tracking-wider">
       {title} {haveSpan ? <span>Partner</span> : null}
     </h2>
