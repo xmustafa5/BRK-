8ce37f0b460abdaad1e360772bd9d1b4874d9860
denamahmud <denamahmud>
2024-02-17 15:50:31 +0300

    i developed  media page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/Home";
-import AboutPage from "./components/About-us";
+import AboutPage from "./components/About-us";  
+import MediaPage from "./components/Media";
+
+
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/aboutus" element={<AboutPage />} />
+        <Route path="/media" element={<MediaPage />} />
         {/* <Route path="/Products" element={Products} />
         <Route path="/Partners" element={Partners} />
         <Route path="/Distributions" element={Distributions} /> */}
