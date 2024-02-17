ae20f3dffbf6e1563dee19be93afcc16c6c68492
Ayoub <124167964+devayoub99@users.noreply.github.com>
2024-02-17 12:51:35 +0300

    Create about us page (Skeleton)

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,13 +4,13 @@ import MainHugeTitle from "../ui/MainHugeTitle";
 import Button from "../ui/Button";
 export default function Home() {
   return (
-    <div className="home relative ">
+    <div className="home relative">
       <div className="flex ml-24 w-full items-center">
         <div className="flex gap-5 flex-col">
           <MainTile title="Industrial Investment" />
-          <MainHugeTitle />
-          <Paragraph 
-          main={true}
+          <MainHugeTitle title="Your Supportive" haveSpan={true} />
+          <Paragraph
+            main={true}
             p="In augue ligula, feugiat ut nulla consequat. Ut est lacus, molestie in
       arcu no, iaculis vehicula ipsum. Nunc faucibus, nisl id dapibus finibus,
       enim diam interdum nulla, sed laoreet risus lectus."
--- src/components/ui/Paragraph.jsx
+++ src/components/ui/Paragraph.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-export default function Paragraph({p ,main=false}) {
-  console.log(main);
+export default function Paragraph({ p, main = false }) {
   return (
-    <p className={`max-w-[500px] text-[16px] ${main  ? 'text-white' : 'text-gray-120'} `}>
+    <p
+      className={`max-w-[500px] text-[16px] ${
+        main ? "text-white" : "text-gray-120"
+      } `}
+    >
       {p}
     </p>
   );
