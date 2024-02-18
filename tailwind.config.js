43b7f46e62a6d6aff50f4d278bc80f57cdefee9a
Ayoub <124167964+devayoub99@users.noreply.github.com>
2024-02-18 10:58:32 +0300

    Add colors to config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        blue: { 
-          '120': '#091242' 
+        blue: {
+          110: "#212b59",
+          120: "#091242",
         },
-        yellow:{
-          '120': '#FABA20'
+        yellow: {
+          120: "#FABA20",
         },
-        black:{
-          '120': '#1C1F35'
+        black: {
+          120: "#1C1F35",
         },
-        gray:{
-          '120':'#666C89'
-        }
-  }
-  },
+        gray: {
+          20: "#f4f4f4",
+          120: "#666C89",
+        },
+      },
+    },
   },
   plugins: [],
 };
