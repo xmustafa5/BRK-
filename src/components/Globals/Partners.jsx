df8d9358524a77e270504f142c38009ed1d57ee1
Mustafa mohammed <107437940+xmustafa5@users.noreply.github.com>
2024-02-18 14:15:53 +0300

    make swiper in partner

--- src/components/Globals/Partners.jsx
+++ src/components/Globals/Partners.jsx
@@ -1,11 +1,80 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { partnerIcons } from "../../assets/svg";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { FreeMode, Pagination, Navigation } from "swiper/modules";
 
 export default function Partners() {
+  const [visiblePartners, setVisiblePartners] = useState([]);
+  const partnersRef = useRef([]);
+
+  const data = [
+    {
+      name: "Partner 1",
+      svg: partnerIcons,
+    },
+    {
+      name: "Partner 2",
+      svg: partnerIcons,
+    },
+    {
+      name: "Partner 3",
+      svg: partnerIcons,
+    },
+    {
+      name: "Partner 4",
+      svg: partnerIcons,
+    },
+    {
+      name: "Partner 5",
+      svg: partnerIcons,
+    },
+  ];
+
+  const [startIndex, setStartIndex] = useState(0);
+
+  const handleNext = () => {
+    setStartIndex((prevIndex) =>
+      prevIndex + 1 <= data.length - 4 ? prevIndex + 1 : prevIndex
+    );
+  };
+
+  const handlePrev = () => {
+    setStartIndex((prevIndex) =>
+      prevIndex - 1 >= 0 ? prevIndex - 1 : prevIndex
+    );
+  };
+  useEffect(() => {
+    const currentVisiblePartners = partnersRef.current.slice(
+      startIndex,
+      startIndex + 4
+    );
+    setVisiblePartners(currentVisiblePartners);
+
+    currentVisiblePartners.forEach((partner) => {
+      partner.classList.add("visible");
+    });
+
+    return () => {
+      partnersRef.current.forEach((partner) => {
+        partner.classList.remove("visible");
+      });
+    };
+  }, [startIndex]);
   return (
-    <div id="Partners" className="flex my-6 justify-center items-center flex-col h-[50vh] gap-6">
-      <div className="bg-gray-200 flex  ">
-        <p className=" font-bold text-yellow-120 flex justify-center items-center bg-yellow-120">
+    <div
+      id="Partners"
+      className="flex my-6 justify-center items-center flex-col h-[50vh] gap-6"
+    >
+      <div className="bg-gray-200 flex">
+        <p className="font-bold text-yellow-120 flex justify-center items-center bg-yellow-120">
           |
         </p>
         <p className="text-md regular text-black-120 font-medium p-1">
@@ -15,27 +84,28 @@ export default function Partners() {
       <h1 className="flex bold flex-col gap-3  text-black-120 text-5xl font-semibold tracking-wider">
         Our Partners
       </h1>
-      <div className="flex  mx-20 flex-wrap gap-4">
-        <div className="mt-5 relative min-w-[200px]   flex-1 w-full">
-          <div className="flex justify-center gap-10 items-center">
-            <div className="flex justify-center items-center flex-col gap-4">
-              {partnerIcons}
-              <p className="text-[20px]">Bdoor Baghdad</p>
-            </div>
-            <div className="flex justify-center items-center flex-col gap-4">
-              {partnerIcons}
-              <p className="text-[20px]">Bdoor Baghdad</p>
-            </div>
-            <div className="flex justify-center items-center flex-col gap-4">
-              {partnerIcons}
-              <p className="text-[20px]">Bdoor Baghdad</p>
-            </div>
-            <div className="flex justify-center items-center flex-col gap-4">
-              {partnerIcons}
-              <p className="text-[20px]">Bdoor Baghdad</p>
-            </div>
-          </div>
-        </div>
+      <div className="w-full h-full flex justify-center items-center">
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={40}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[FreeMode, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>{partnerIcons}</SwiperSlide>
+          <SwiperSlide>{partnerIcons}</SwiperSlide>
+          <SwiperSlide>{partnerIcons}</SwiperSlide>
+          <SwiperSlide>{partnerIcons}</SwiperSlide>
+          <SwiperSlide>{partnerIcons}</SwiperSlide>
+          <SwiperSlide>{partnerIcons}</SwiperSlide>
+          <SwiperSlide>{partnerIcons}</SwiperSlide>
+          <SwiperSlide>{partnerIcons}</SwiperSlide>
+          <SwiperSlide>{partnerIcons}</SwiperSlide>
+        </Swiper>
       </div>
       <button className="Regular mt-2 bg-blue-120 w-fit px-8 py-2 text-white rounded-[0px]">
         Show All Partners
