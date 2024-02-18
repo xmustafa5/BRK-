c71300fbda0d5d3d1333df750d0a240c0c0db09e
Ayoub <124167964+devayoub99@users.noreply.github.com>
2024-02-18 13:43:27 +0300

    Responsive About section => About page.

--- src/components/Globals/About.jsx
+++ src/components/Globals/About.jsx
@@ -10,40 +10,31 @@ import SectionTitle from "../ui/Section-title";
 
 export default function About({ includeVision }) {
   return (
-    <div className="flex h-[85vh]" id="about">
-      <div className="flex justify-center items-center w-[50vw]">
-        <div className="relative">
-          <img src={imaging} className="min-w-[540px]" alt="" />
-          <img
-            src={mainImage}
-            alt=""
-            className="absolute min-w-[200px] -bottom-16 -right-16 border-[16px] border-white"
-          />
+    <section id="about" className="py-20 lg:py-0">
+      <div className="container min-h-[85vh] gap-8 flex flex-col-reverse lg:flex-row lg:items-center lg:justify-center">
+        <div className="flex justify-center items-center lg:w-[50vw]">
+          <div className="relative flex lg:mr-24">
+            <img
+              src={imaging}
+              className="w-[300px] md:w-[450px] lg:min-w-[540px]"
+              alt="Factory"
+            />
+            <img
+              src={mainImage}
+              alt="Factory 2"
+              className="hidden lg:block absolute min-w-[200px] -bottom-16 -right-16 border-[16px] border-white"
+            />
+          </div>
         </div>
-      </div>
-      <div className="flex justify-center gap-5 items-start flex-col">
-        <MainTitle title="Company overview" />
-        <SectionTitle title="About Us" />
-        <Paragraph
-          main={false}
-          p="Leverage agile frameworks to provide a robust synopsis for strategy
+        <div className="flex flex-col justify-center items-center gap-4 text-center lg:items-start lg:text-start">
+          <MainTitle title="Company overview" />
+          <SectionTitle title="About Us" />
+          <Paragraph
+            main={false}
+            p="Leverage agile frameworks to provide a robust synopsis for strategy
           foster collaborative thinking to further the overall value
           proposition."
-        />
-        <Paragraph
-          main={false}
-          p="Sed ut perspiciatis, unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt, explicabo. Nemo enim ipsam voluptatem quia voluptassit."
-        />
-        {includeVision ? (
-          <>
-            <h4 className="text-black-120 bold text-[16px]">Vision</h4>
-            <SubTitle title="Establishing presence locally, and launch towards markets globalism" />
-            <Button title="About Us" />
-          </>
-        ) : (
+          />
           <Paragraph
             main={false}
             p="Sed ut perspiciatis, unde omnis iste natus error sit voluptatem
@@ -51,8 +42,23 @@ export default function About({ includeVision }) {
           ab illo inventore veritatis et quasi architecto beatae vitae dicta
           sunt, explicabo. Nemo enim ipsam voluptatem quia voluptassit."
           />
-        )}
+          {includeVision ? (
+            <>
+              <h4 className="text-black-120 bold text-[16px]">Vision</h4>
+              <SubTitle title="Establishing presence locally, and launch towards markets globalism" />
+              <Button title="About Us" />
+            </>
+          ) : (
+            <Paragraph
+              main={false}
+              p="Sed ut perspiciatis, unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt, explicabo. Nemo enim ipsam voluptatem quia voluptassit."
+            />
+          )}
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
