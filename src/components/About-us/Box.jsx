65dee2152d957742cbea526ff6dfdb696007862e
Ayoub <124167964+devayoub99@users.noreply.github.com>
2024-02-18 15:09:11 +0300

    Enhance Box component

--- src/components/About-us/Box.jsx
+++ src/components/About-us/Box.jsx
@@ -1,8 +1,8 @@
 export default function Box({ title, imgPath, p }) {
   return (
-    <div className="flex flex-col items-center gap-[22px] w-80 mx-atuo">
+    <div className="flex flex-col items-center gap-[22px] w-80 mx-auto">
       <img src={imgPath} alt="Factory" />
-      <h3 className="font-bold text-[25px]">{title}</h3>
+      <h3 className="font-bold text-[20px] sm:text-[25px]">{title}</h3>
       {p && <p className="text-center">{p}</p>}
     </div>
   );
