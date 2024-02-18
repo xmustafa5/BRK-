32b0c12c2939f4f760c3cd6766eef34c62a5e656
Ayoub <124167964+devayoub99@users.noreply.github.com>
2024-02-18 15:10:41 +0300

    Enhance DarkLabel, SectionTitle components

--- src/components/ui/DarkLabel.jsx
+++ src/components/ui/DarkLabel.jsx
@@ -1,6 +1,6 @@
 export default function DarkLabel({ title }) {
   return (
-    <div className="bg-[#cf9e2c] flex w-fit pr-2">
+    <div className="bg-[#cf9e2c] flex w-fit pr-2 text-sm sm:text-base ">
       <span className="block w-1 bg-blue-110"></span>
       <p className="regular pl-1 py-1 uppercase bold text-blue-110">{title}</p>
     </div>
