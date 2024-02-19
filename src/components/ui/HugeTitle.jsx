5106b4fae459f1a07307c78421c8a6d315123051
denamahmud <denamahmud>
2024-02-19 15:05:07 +0300

    Updated media page

--- src/components/ui/HugeTitle.jsx
+++ src/components/ui/HugeTitle.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function HugeTitle({ title }) {
   return (
-    <h2 className="flex bold flex-col gap-3  text-black-120 text-5xl font-semibold tracking-wider mt-6">
+    <h2 className="text-white flex bold flex-col gap-3   text-5xl font-semibold tracking-wider mt-6">
       {title}
     </h2>
   );
